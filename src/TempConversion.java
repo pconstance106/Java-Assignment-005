6f10be580a70119a5c0bc87b04f9cfc7d2ae3772
netlab-student <pconstance106@mycr.redwoods.edu>
2022-11-02 12:12:28 -0700

    Completed Fall-2022

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TempConversion {
-    static Scanner input = new Scanner(System.in);
+    static final Scanner input = new Scanner(System.in);
     public static double convertC2F(double celsius) {
         return (celsius * (9.0/5)) + 32.0;
     }
@@ -17,15 +17,13 @@ public class TempConversion {
     public static double convertK2C(double kelvin) {
         return kelvin - 273.15;
     }
-    public static double convertK2F(double kelvin) {
-        return convertC2F(convertK2C(kelvin));
-    }
+
+    public static double convertK2F(double kelvin) { return convertC2F(convertK2C(kelvin));}
 
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
     }
-
     public static String getUnitChoice() {
         System.out.printf("%-4s : %40s%n", "C).", "To convert a °C temperature");
         System.out.printf("%-4s : %40s%n", "F).", "To convert a °F temperature");
@@ -36,7 +34,41 @@ public class TempConversion {
 
     public static void main(String[] args) {
         while(true) {
+            System.out.println("What do you want to convert from?");
+            String convertFrom = getUnitChoice();
+            if (convertFrom.equals("Q") || convertFrom.equals("q")) {
+                return;
+            }
 
+            System.out.println("What do to want to convert to?");
+            String convertTo = getUnitChoice();
+            if (convertTo.equals("Q") || convertTo.equals("q")) {
+                return;
+            }
+
+            double temp = getTemp(convertFrom);
+            String conversion = convertFrom + convertTo;
+
+            switch (conversion) {
+                case "CF":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertC2F(temp) + convertTo + "\n");
+                    break;
+                case "CK":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertC2K(temp) + convertTo + "\n");
+                    break;
+                case "FC":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertF2C(temp) + convertTo + "\n");
+                    break;
+                case "FK":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertF2K(temp) + convertTo + "\n");
+                    break;
+                case "KC":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertK2C(temp) + convertTo + "\n");
+                    break;
+                case "KF":
+                    System.out.println("\nResult: " + temp + convertFrom + " is " + convertK2F(temp) + convertTo + "\n");
+                    break;
+            }
         }
     }
 }
